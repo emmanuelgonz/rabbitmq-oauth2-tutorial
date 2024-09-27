e30e5ea7b9aecaafb200ca4395fc8d24e396e9ca
emmanuelgonz <emmanuelgonzalez@asu.edu>
2024-09-27 18:42:19 +0000

    clean start

--- start.sh
+++ start.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
 # Start Keycloak
-#sudo make start-keycloak
+sudo make start-keycloak
 
 # Start RabbitMQ
 export MODE=keycloak
 sudo make start-rabbitmq
-
-# Start TCP relay
-#sudo docker run -d -p 15670:15670 --net rabbitmq_net cloudamqp/websocket-tcp-relay --upstream tls://98.83.140.219:5671 --bind=98.83.140.219 --tls-cert=/conf/certs_private/basic/server_localhost/cert.pem --tls-key=/conf/certs_private/basic/server_localhost/key.pem
