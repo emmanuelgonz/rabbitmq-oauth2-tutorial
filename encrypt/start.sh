17ca0e9f8f3e1902aabac780991b208442f6ee49
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2024-10-07 15:19:49 -0700

    clean dir

--- encrypt/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# sudo docker network create localtest
-docker-compose up -d
-# sudo docker-compose logs certbot
-
-# echo "0 3 * * * docker-compose -f /path/to/your/docker-compose.yml run --rm certbot renew --quiet" | sudo tee -a /etc/crontab
-# sudo systemctl restart cron
