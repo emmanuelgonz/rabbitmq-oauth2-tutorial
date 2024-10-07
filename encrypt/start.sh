9a9debdc67af19cd0b28a23cc7ae37400d8e14df
EC2 Default User <ec2-user@ip-172-31-65-96.ec2.internal>
2024-10-07 20:04:29 +0000

    changes

--- encrypt/start.sh
+++ encrypt/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # sudo docker network create localtest
-sudo docker-compose up -d
+docker-compose up -d
 # sudo docker-compose logs certbot
 
 # echo "0 3 * * * docker-compose -f /path/to/your/docker-compose.yml run --rm certbot renew --quiet" | sudo tee -a /etc/crontab
